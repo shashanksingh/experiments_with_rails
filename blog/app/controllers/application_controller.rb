fcc7f20debb24b7a1f15688683a96ace0354ef3c
shashank singh <shashank.personal@gmail.com>
2013-05-07 18:25:49 +0530

    blog commit are working

--- blog/app/controllers/application_controller.rb
+++ blog/app/controllers/application_controller.rb
@@ -1,3 +1,22 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  def index
+  	@post = Post.find(params[:post_id])
+  	#@comment = @post.all
+  end
+
+  def create
+  	@post = Post.find(params[:post_id])
+  	@comment = @post.comments.create(params[:comment])
+  	redirect_to post_path(@post)
+  end
+
+  def show
+  	@post = Post.find(params[:post_id])
+  	@comment = @post.comments.create(params[:comment])
+  end
+
+
+
 end
