28c159c229c801becad0fc1f4a14b4c0c4bf30c5
tania dey <tania@shashank.singh>
2013-05-07 16:02:50 +0530

    created a stock ruby on rails application

--- README.md
+++ README.md
@@ -2,3 +2,7 @@ experiments_with_rails
 ======================
 
 Ruby when "she" stared travelling on rails, left a python wrangler behind .
+
+
+Tue May  7 15:59:11 IST 2013
+My first ruby on rails project, ever to understand ruby on rails , inspiration is : Rails for Zombies => http://railsforzombies.org
---
+++ blog/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = '7dbbb40b303973f958b5927c8701e71beadf62b1f10a44251fef67caa66da747c00374b90c4f87f540570628c1dca37ff366d71dd84c46c569744551fd6b4288'
